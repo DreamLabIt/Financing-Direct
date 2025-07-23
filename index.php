729f1f69e12e5c2fd5f5af188e84533d5b4cff1e
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-23 22:57:43 +0600

    update header section

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Financing Direct | Home</title>
-  <link rel="stylesheet" href="./assets/style.css">
-</head>
-<body>
-  <h1>This is Financing diret project</h1>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Financing Direct | Home</title>
+    <link rel="stylesheet" href="./assets/style.css">
+  </head>
+  <body>
+    <main> 
+      <!-- ====  Beginning header section === -->
+      <header class="header-wrap">
+        <!-- header top -->
+          <div class="common-wrap flex-box">
+            <div>
+              <a href="#">
+                <img src="./assets/images/home-financing-direct-logo.png " alt="logo">
+              </a>
+            </div>
+            <div class="flex-box-all call-wrap">
+              <p class="">
+                <span class="call-title">Call Us Today!</span>
+                <span >(512) 593-4157</span>
+              </p>
+              <button class="btn">Get a Rate Quote</button>
+            </div>
+          </div> 
+         <!-- header bottom -->
+          <nav class="nav-wrap">
+            <ul class="common-wrap flex-box">
+              <li><a href="#">Mortgages</a></li>
+              <li><a href="#">Find a Loan Officer</a></li>
+              <li><a href="#">Testimonials  </a></li>
+              <li><a href="#">Resources</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact  Us</a></li>
+            </ul>
+          </nav>
+      </header>
+    </main>
+  </body>
 </html>

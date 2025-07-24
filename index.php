f17262a750d4fcef362b4284a3c3591c1ef414c4
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-24 12:47:51 +0600

    update footer

--- index.php
+++ index.php
@@ -37,6 +37,182 @@
             </ul>
           </nav>
       </header>
+     <!-- ====  End header section === -->
+
+
+      <!-- ====  Beginning footer section === -->
+
+        <footer class="footer-wrap">
+          <div class="offer-wrap flex-box-all">
+            <p>Curious how low of a rate we can offer you? Get a <span>free rate quote</span> today!</p>
+          </div>
+          <div class="common-wrap flex">
+            <div class="widget-item">
+
+                <div class="footer-logo">
+
+                    <a href="https://www.homefinancingdirect.com/">
+                        <img src="https://www.homefinancingdirect.com/img/hfd-logo-footer.png" alt="hfd logo footer">
+                    </a>
+
+                </div>
+
+                <address>11701 FM Rd 2244 Suite 140<br> Austin, TX 78738</address>
+
+
+                <a class="phone" href="tel:512-593-4157" >(512) 593-4157</a>
+
+                <a  class="email" href="mailto:info@homefinancingdirect.com">info@homefinancingdirect.com</a>
+
+            </div>
+            <div class="widget-item-links">
+              <h5>Quick Links</h5>
+
+                <div class="flex-box">
+                  <ul>
+                    <li><a href="mortgage-rates-today">Mortgage Rates</a></li>
+                    <li><a href="get-preapproved">Get Preapproved</a></li>
+                    <li><a href="types-of-mortgages">Types of Mortgages</a></li>
+                    <li><a href="zero-down-mortgages">Zero Down Mortgages</a></li>
+                    <li><a href="jumbo-loans">Jumbo Loans</a></li>
+                    <li><a href="mortgage-calculator">Mortgage Calculator</a></li>           
+                  </ul>
+                  <ul>
+                    <li><a href="testimonials">Testimonials</a></li>
+                    <li><a href="mortgage-resources">Mortgage Resources</a></li>
+                    <li><a href="about-us">About Us</a></li>
+                    <li><a href="contact-us">Contact Us</a></li>
+                    <li><a href="rate-alerts">Get Rate Alerts</a></li>
+                    <li><a href="img/privacy-policy-terms-conditions.pdf" target="_blank">Privacy/Terms & Conditions</a></li>
+
+                  </ul>
+                </div>
+            </div>
+            <div class="widget-item">
+              <h5>Newsletter</h5>
+
+              <div class="quotes-btn-wrap">
+              <form id="subscribe-form">
+                  <div class="container mail-input" style="background-color:white">
+                      <input placeholder="Your Email" type="email" id="email" name="email" required>
+                  </div>
+                  <div class="container dec-submit-cta">
+                      <input type="submit" value="SUBMIT">
+                  </div>
+              </form>
+              <div id="thank-you-message" style="display: none; color: #fff; margin-top: 10px;">
+                  Thank you for subscribing
+              </div>
+
+
+              </div>
+            </div>
+          </div>
+        </footer> 
+       <!-- ====  End footer section ===-->
+
+      
+<!-- <footer class="main-footer-section dec-main-footer">
+
+<div class="common-wrap flex">
+
+    <div class="widget-item">
+
+        <div class="footer-logo">
+
+            <a href="https://www.homefinancingdirect.com/">
+                <img src="https://www.homefinancingdirect.com/img/hfd-logo-footer.png" alt="hfd logo footer">
+            </a>
+
+        </div>
+
+        <address>11701 FM Rd 2244 Suite 140<br> Austin, TX 78738</address>
+
+
+        <a class="phone" href="tel:512-593-4157" >(512) 593-4157</a>
+
+        <a  class="email" href="mailto:info@homefinancingdirect.com">info@homefinancingdirect.com</a>
+
+    </div>
+
+    <div class="widget-item">
+
+        <h5>Quick Links</h5>
+
+        <ul>
+
+            <li><a href="mortgage-rates-today">Mortgage Rates</a></li>
+            <li><a href="get-preapproved">Get Preapproved</a></li>
+			<li><a href="types-of-mortgages">Types of Mortgages</a></li>
+			<li><a href="zero-down-mortgages">Zero Down Mortgages</a></li>
+			<li><a href="jumbo-loans">Jumbo Loans</a></li>
+            <li><a href="mortgage-calculator">Mortgage Calculator</a></li>           
+        </ul>
+         <ul>
+			<li><a href="testimonials">Testimonials</a></li>
+			<li><a href="mortgage-resources">Mortgage Resources</a></li>
+	   	    <li><a href="about-us">About Us</a></li>
+            <li><a href="contact-us">Contact Us</a></li>
+	        <li><a href="rate-alerts">Get Rate Alerts</a></li>
+			<li><a href="img/privacy-policy-terms-conditions.pdf" target="_blank">Privacy/Terms & Conditions</a></li>
+
+        </ul>
+
+    </div>
+
+    <div class="widget-item">
+        
+
+        <h5>Newsletter</h5>
+
+        <div class="quotes-btn-wrap">
+        <form id="subscribe-form">
+            <div class="container mail-input" style="background-color:white">
+                <input placeholder="Your Email" type="email" id="email" name="email" required>
+            </div>
+            <div class="container dec-submit-cta">
+                <input type="submit" value="SUBMIT">
+            </div>
+        </form>
+        <div id="thank-you-message" style="display: none; color: #fff; margin-top: 10px;">
+            Thank you for subscribing
+        </div>
+
+
+        </div>
+
+    </div>
+
+</div> -->
+
+<!-- <div class="footer-bottom">
+
+    <div class="common-wrap clear">
+
+        <div class="footer-bottom-item">
+
+            <p> &copy; &nbsp; 2025 Home Financing Direct</p>
+
+        </div>
+
+        <div class="footer-bottom-item">
+
+            <p>NMLS ID: 1948000</p>
+
+        </div>
+
+        <div class="footer-bottom-item">
+
+            <p><a href="https://www.nmlsconsumeraccess.org/EntityDetails.aspx/COMPANY/1948000" target="_blank">NMLS Consumer Access</a></p>
+
+        </div>
+        
+
+    </div>
+
+</div> -->
+
+<!-- </footer> -->
     </main>
   </body>
 </html>

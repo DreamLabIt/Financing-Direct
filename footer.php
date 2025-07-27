afb59fdeeb5f25ad637b6e4e44855895ff87ddc3
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-27 15:42:33 +0600

    update footer section

--- footer.php
+++ footer.php
@@ -1,71 +1,75 @@
 <!-- ====  Beginning footer section === -->
 
 
-        <footer class="footer-wrap">
-          <div class="offer-wrap">
-            <div class="offer common-wrap">
-              <div class="image-wrap">
-                <img src="./assets/icon/persent-icon.png" alt="persent icon">
-              </div>
-                
-                <p>Curious how low of a rate we can offer you? Get a <span>free rate quote</span> today!</p>
-            </div>
-            
+<footer class="footer-wrap">
+  <div class="offer-wrap">
+    <div class="common-wrap">
+      <div class="offer">
+        <h4>
+          <img src="./assets/icon/persent-icon.png" alt="persent icon"> Curious how low of a rate we can offer you? Get a <span>free rate quote</span> today!</h4>
+      </div>
+    </div>
+  </div>
+  <div class="common-wrap flex footer-bottom">
+    <div class="widget-item address-wrap">
+      <div class="footer-logo">
+          <a href="https://www.homefinancingdirect.com/">
+              <img src="./assets/images/home-financing-direct-logo.png " alt="logo">
+          </a>
+      </div>
+      <address>11701 FM Rd 2244 Suite 140<br> Austin, TX 78738</address>
+      <a class="phone" href="tel:512-593-4157" >(512) 593-4157</a>
+      <a  class="email" href="mailto:info@homefinancingdirect.com">info@homefinancingdirect.com</a>
+    </div>
+    <div class="widget-item-links">
+      <h5>Quick Links</h5>
+        <div class="links d-flex">
+          <ul>
+            <li><a href="mortgage-rates-today">Mortgage Rates</a></li>
+            <li><a href="get-preapproved">Get Preapproved</a></li>
+            <li><a href="types-of-mortgages">Types of Mortgages</a></li>
+            <li><a href="zero-down-mortgages">Zero Down Mortgages</a></li>
+            <li><a href="jumbo-loans">Jumbo Loans</a></li>
+            <li><a href="mortgage-calculator">Mortgage Calculator</a></li>           
+          </ul>
+          <ul>
+            <li><a href="testimonials">Testimonials</a></li>
+            <li><a href="mortgage-resources">Mortgage Resources</a></li>
+            <li><a href="about-us">About Us</a></li>
+            <li><a href="contact-us">Contact Us</a></li>
+            <li><a href="rate-alerts">Get Rate Alerts</a></li>
+            <li><a href="img/privacy-policy-terms-conditions.pdf" target="_blank">Privacy/Terms & Conditions</a></li>
+          </ul>
+        </div>
+    </div>
+    <div class="widget-item">
+      <h5>Newsletter</h5>
+        <form class="form-row d-flex flex-wrap gap-10" >
+          <input type="text" />
+          <button class="btn" type="submit">SUBMIT</button>
+        </form>
+    </div>
+  </div>
+  
+  <div class="copy-right">
+    <div class="common-wrap">
+      <div class="d-flex item-center">
+        <div class="item-left">
+          <p> &copy; &nbsp; 2025 Home Financing Direct</p>
+        </div>
+        <div class="item-right d-flex item-center justify-between">
+          <div class="item">
+            <p>NMLS ID: 1948000</p>
           </div>
-          <div class="common-wrap flex footer-bottom">
-            <div class="widget-item">
-
-                <div class="footer-logo">
-
-                    <a href="https://www.homefinancingdirect.com/">
-                        <img src="./assets/images/home-financing-direct-logo.png " alt="logo">
-                    </a>
-
-                </div>
-
-                <address>11701 FM Rd 2244 Suite 140<br> Austin, TX 78738</address>
-
-
-                <a class="phone" href="tel:512-593-4157" >(512) 593-4157</a>
-
-                <a  class="email" href="mailto:info@homefinancingdirect.com">info@homefinancingdirect.com</a>
-
-            </div>
-            <div class="widget-item-links">
-              <h5>Quick Links</h5>
-
-                <div class="links flex-box">
-                  <ul>
-                    <li><a href="mortgage-rates-today">Mortgage Rates</a></li>
-                    <li><a href="get-preapproved">Get Preapproved</a></li>
-                    <li><a href="types-of-mortgages">Types of Mortgages</a></li>
-                    <li><a href="zero-down-mortgages">Zero Down Mortgages</a></li>
-                    <li><a href="jumbo-loans">Jumbo Loans</a></li>
-                    <li><a href="mortgage-calculator">Mortgage Calculator</a></li>           
-                  </ul>
-                  <ul>
-                    <li><a href="testimonials">Testimonials</a></li>
-                    <li><a href="mortgage-resources">Mortgage Resources</a></li>
-                    <li><a href="about-us">About Us</a></li>
-                    <li><a href="contact-us">Contact Us</a></li>
-                    <li><a href="rate-alerts">Get Rate Alerts</a></li>
-                    <li><a href="img/privacy-policy-terms-conditions.pdf" target="_blank">Privacy/Terms & Conditions</a></li>
-
-                  </ul>
-                </div>
-            </div>
-            <div class="widget-item">
-              <h5>Newsletter</h5>
-
-                <form class="flex">
-                  <div class="form-row">
-                    <input type="text" placeholder="Enter your text" />
-                    <button type="submit">SUBMIT</button>
-                  </div>
-                </form>
-
-
-            </div>
+          <div class="item">
+            <p><a href="https://www.nmlsconsumeraccess.org/EntityDetails.aspx/COMPANY/1948000" target="_blank">NMLS Consumer Access</a>
+            </p>
           </div>
-        </footer> 
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+</footer> 
        <!-- ====  End footer section ===-->

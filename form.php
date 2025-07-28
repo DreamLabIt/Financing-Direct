64a2080ee12f2b7e1f2737e1437bf6612b4e0f1f
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-07-28 12:24:31 +0600

    form update

--- form.php
+++ form.php
@@ -1,36 +1,40 @@
-<div class="common-wrap common-wrap-form">
+<!-- <div class="common-wrap common-wrap-form">
   <div class="help">
     <h3 class="title">We’re Here to Help</h3>
     <p>Have a mortgage related question? Looking for additional information about our mortgage products and services? Please reach out to us anytime!</p>
     <button class="btn"><img src="./assets/images/phone-icon.png" alt=""> (512) 593-4157</button>
   </div>
- <div class="form-box d-flex flex-col gap-20">
-  <div class="form-title text-center">
-    <h2>Ask Us a Question</h2>
-    <p>Get expert mortgage advice</p>
-  </div>
-      <form>
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" id="name">
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email">
-        </div>
-        <div class="form-group">
-          <label for="phone">Phone</label>
-          <input type="text" id="phone">
-        </div>
-        <div class="form-group">
-          <label for="contact">Preferred Method of Contact</label>
-          <input type="text" id="contact" >
-        </div>
-        <div class="form-group full">
-          <label for="message">Question or Comment</label>
-          <textarea id="message" rows="5"></textarea>
-        </div>
-        <button type="submit" class="submit-btn">✉️ SEND</button>
-      </form>
+ 
+ </div> -->
+
+<section class="common-form-section">
+  <div class="common-form-wrap d-flex flex-col">
+    <div class="common-form-content text-center">
+      <h2>Ask Us a Question</h2>
+      <p>Get expert mortgage advice</p>
     </div>
- </div>
+    <form>
+      <div class="common-form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name">
+      </div>
+      <div class="common-form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email">
+      </div>
+      <div class="common-form-group">
+        <label for="phone">Phone</label>
+        <input type="text" id="phone">
+      </div>
+      <div class="common-form-group">
+        <label for="contact">Preferred Method of Contact</label>
+        <input type="text" id="contact" >
+      </div>
+      <div class="common-form-group wrap">
+        <label for="message">Question or Comment</label>
+        <textarea id="message" rows="5"></textarea>
+      </div>
+      <button type="submit" class="submit-btn"><img src="./assets/images/send-icon.png" alt="send icon"> SEND</button>
+    </form>
+  </div>
+</section>
